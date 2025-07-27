945acbe09a1f0ea8a762f4382c32ce682718a9b2
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2025-07-27 15:20:32 +1000

    (silver) set `wezterm` opacity to 100%

--- silver/.wezterm.lua
+++ silver/.wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.enable_scroll_bar = true
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.75
+config.window_background_opacity = 1.00
 
 config.font = wezterm.font_with_fallback {
     "Fira Code",
